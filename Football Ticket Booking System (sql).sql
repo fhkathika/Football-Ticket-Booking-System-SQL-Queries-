c3e2ac46bd2eb0752ff3003ca57d92b63889b880
fhkathika <fhkathika057@gmail.com>
2026-06-19 12:29:43 +0600

    added quire 1, 2

--- Football Ticket Booking System (sql).sql
+++ Football Ticket Booking System (sql).sql
@@ -69,4 +69,17 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 
 
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+select match_id,fixture,base_ticket_price from Matches as m
+where m.tournament_category='Champions League' and m.match_status='Available'
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+select user_id,full_name,email from Users as u
+where u.full_name like'Tanvir%' or u.full_name ilike'%Haque%'
+
+
+
+
+
 
